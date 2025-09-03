3aa76525ede68aace334500101c9b7a6b8b42f68
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-03 20:37:03 +0530

    done initilization

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='font-bold text-5xl text-center flex justify-center items-center h-screen'>
+      Modern Tailwind CSS
+    </div>
+  )
+}
+
+export default App
