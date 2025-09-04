9724e5f622f88e1f31bbcba9651f5302ffcaca1c
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-04 21:38:23 +0530

    container

--- tailwind/src/Components/Hero.jsx
+++ tailwind/src/Components/Hero.jsx
@@ -3,9 +3,16 @@ import { Container } from './Container'
 import Nav from './Nav'
 function Hero() {
   return (
-    <Container className="flex items-center justify-center h-screen">
+    <div className='h-screen relative bg-blue-100'>
+
+      <div className='max-w-7xl mx-auto absolute inset-0 h-full w-full'>
+        <div className='h-full absolute inset-y-0 left-0 w-px bg-gradient-to-b from-neutral-400/50 via-neutral-200 to-transparent'></div>
+        <div className='h-full absolute inset-y-0 right-0 w-px bg-gradient-to-b from-neutral-400/50 via-neutral-200 to-transparent'></div>
+      </div>
+    <Container className="flex items-start justify-center">
       <Nav/>
     </Container>
+    </div>
   )
 }
 
