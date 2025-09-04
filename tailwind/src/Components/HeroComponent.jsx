e6647129242ae2d17854a5e320864528e5d1923e
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-04 21:59:57 +0530

    done

---
+++ tailwind/src/Components/HeroComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HeroComponent() {
+  return (
+    <div className='px-4 py-2 flex items-center flex-col w-full'>
+        <button className='border border-neutral-200 bg-neutral-100 text-neutral-600 hover:bg-gray-200 transition duration-200 cursor-pointer rounded-full flex items-center px-8 py-[3px] text-sm shadow-xm '>
+            Incorporation common mistakes to avoid
+        </button>
+    </div>
+  )
+}
+
+export default HeroComponent
